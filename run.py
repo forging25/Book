d694a208839881d929e7ade7aaa70bca85aa1f91
时涛涛 <1658751131@qq.com>
2019-05-22 15:40:18 +0800

    simple

---
+++ run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+ Created by howie.hu at 2018/8/13.
+"""
+import os
+import subprocess
+
+if __name__ == '__main__':
+    # os.environ['MODE'] = 'PRO'
+    servers = [
+        ["pipenv", "run", "gunicorn", "-c", "config/dev_gunicorn.py", "--worker-class", "sanic.worker.GunicornWorker",
+         "server:app"],
+        ["pipenv", "run", "python", "scheduled_task.py"]
+    ]
+    procs = []
+    for server in servers:
+        proc = subprocess.Popen(server)
+        procs.append(proc)
+    for proc in procs:
+        proc.wait()
+        if proc.poll():
+            exit(0)
